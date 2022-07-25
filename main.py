93af53adb5b840d9c3e9b1ed0640e95c5c7650de
Ishan Rastogi <101089388+Ishan2608@users.noreply.github.com>
2022-07-25 09:02:44 +0530

    Code Improvements

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def upload_file():
     filename = filedialog.askopenfilename(initialdir=FILE_PATH, title="Select Your Image")
     file_upload_btn.config(fg='green')
     f = filename.split('/')[-1]
-    uploaded_file_name.config(text=f)
+    uploaded_file_name.config(text=f"File Name: {f}")
 
 
 def water_mark():
@@ -30,13 +30,14 @@ def water_mark():
 
     w, h = main_image.size
     x, y = int(w / 2), int(h / 2)
-    font = ImageFont.truetype("arial.ttf", int(w / 7))
+    font = ImageFont.truetype("arial.ttf", int(w / 6))
 
     draw.text((x, y), text, font=font, fill=(255, 255, 255, 170), anchor='ms')
     final = Image.alpha_composite(main_image, watermark)
 
     f = filename.split('/')[-1]
     rand_id = random.random() * 1000000
+    rand_id = int(rand_id)
     new_path = f'{f}_watermarked_{rand_id}.png'
     final.save(new_path)
     final.show()
@@ -61,7 +62,7 @@ water_text = tk.Entry(fg="white", bg='black', font=('Arial', 12, 'bold'))
 file_upload_btn = tk.Button(text="Upload", font=('Arial', 12, 'bold'), command=upload_file)
 submit_btn = tk.Button(text='Submit', font=('Arial', 12, 'bold'), command=water_mark)
 
-uploaded_file_name = tk.Label(text=" ", fg='white', bg='black',
+uploaded_file_name = tk.Label(text="File Name: ", fg='white', bg='black',
                               font=('Arial', 12, 'bold'), wraplength=500)
 
 canvas.grid(row=0, column=0, rowspan=18, columnspan=4)
